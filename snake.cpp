33b66e263597840e0a74a8cbc605cfd40dd56b75
zahid ali <za2206129@gmail.com>
2025-11-19 22:47:55 +0530

    Loading images

--- snake.cpp
+++ snake.cpp
@@ -1,16 +1,37 @@
 #include "raylib.h"
 
 #include<iostream>
-
+Color green={173 , 204 , 96 , 255};
+Color darkgreen={43 , 51 , 24, 255};
 using namespace std;
+int cellSize=30;
+int cellCount=25;
 
+class Food{
+  public:
+  Vector2 position={5 , 6};
+  Food()
+  {
+    // Image is type of data structure in raylib that contains the pixels data of the graphical image 
+    Image image=LoadImage("./Graphics/Images.jpg"); 
+  }
+  void draw()
+  {
+    DrawRectangle(position.x * cellSize , position.y * cellSize , cellSize , cellSize ,WHITE);
+  }
+};
+Food food;
 int main()
 {
-    int screen_wdith=1200; int screen_height=1000;
-  InitWindow(screen_wdith , screen_height , "My snake game");
+    int screen_wdith=750; int screen_height=750;
+    SetTargetFPS(60);
+  InitWindow(cellSize * cellCount , cellSize *cellCount , "My snake game");
   while (!WindowShouldClose())
   {
     BeginDrawing();
+    ClearBackground(green);
+    food.draw();
+    
     EndDrawing();
   }
   
